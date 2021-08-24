d953c1a79ebbf0fbc063af4b29f0d7636112cb80
Ido Lempert <ido.lempert@poalim.co.il>
2021-08-24 16:04:58 +0300

    4

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const app = new express();
 const port = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
-    res.setHeader('Report-To', '{"group":"default","max_age":31536000,"endpoints":[{"url":"https://13588727568c1c68ab7fa56e96ca7723.report-uri.com/a/d/g"}],"include_subdomains":true}');
+    res.setHeader('Report-To', '{"group":"default","max_age":31536000,"endpoints":[{"url":"https://aqueous-atoll-05115.herokuapp.com/ng-portals/report-violation"}],"include_subdomains":true}');
     res.setHeader('NEL','{"report_to":"default","max_age":31536000,"include_subdomains":true,"success_fraction": 1.0,"failure_fraction": 1.0}');
 
     next();
 });
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
